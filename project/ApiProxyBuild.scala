b682b43e468ba3ca08617d74ccc91ec564ff8dd8
Mark Janssen <mark@praseodym.net>
2013-03-07 20:34:51 +0100

    Cleanup

--- project/ApiProxyBuild.scala
+++ project/ApiProxyBuild.scala
@@ -15,9 +15,10 @@ object ApiProxyBuild extends Build {
       libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.1.1",
       libraryDependencies += "com.typesafe.akka" % "akka-slf4j_2.10" % "2.1.1",
       resolvers += "spray repo" at "http://repo.spray.io",
+      libraryDependencies += "io.spray" % "spray-can" % "1.1-M7",
       libraryDependencies += "io.spray" % "spray-client" % "1.1-M7",
       libraryDependencies += "io.spray" % "spray-caching" % "1.1-M7",
       libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.9"
-  )
+    )
   )
 }
